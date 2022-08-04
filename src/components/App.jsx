41d66e163b71e2a01fc39ebb62174c0b9fd25b2f
Mauro Reis <mauroacsreis@gmail.com>
2022-08-04 17:44:10 -0300

    First Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+var isDone = false;
+
+function strike() {
+  //// Imperative
+  document.getElementById("root").style.textDecoration = "line-through";
+  //// Declarative
+  // isDone = true;
+}
+
+function unStrike() {
+  //// Imperative
+  document.getElementById("root").style.textDecoration = null;
+  //// Declarative
+  // isDone = false;
+}
+
+function App() {
+  return (
+    <div>
+      <p style={isDone ? { textDecoration: "line-through" } : null}>Buy milk</p>
+      <button onClick={strike}>Change to strike through</button>
+      <button onClick={unStrike}>Change back</button>
+    </div>
+  );
+}
+
+export default App;
